516589fbfdc9566ea0551d88133f7dcfda353901
Martina Mele <80338879+Darkangel04@users.noreply.github.com>
2024-04-17 20:11:57 +0200

    Delete src/main directory

--- src/main/java/DAO/UtenteDAO.java
+++
@@ -1,9 +0,0 @@
-package DAO;
-
-import Model.Utente;
-
-public interface UtenteDAO {
-    public boolean validateLogin(Utente U);         // Metodo per validare le credenziali di accesso di un utente
-    public boolean  validateRegistration(Utente U); // Metodo per validare la registrazione di un nuovo utente
-    public boolean deleteUtente(Utente U);          // Metodo per eliminare un utente dalla tabella Utente
-}
--- src/main/java/DAO/VisitaDAO.java
+++
@@ -1,7 +0,0 @@
-package DAO;
-
-import Model.Visita;
-
-public interface VisitaDAO {
-    public void insertView(Visita V);   // Metodo per inserire una nuova visita nel database
-}
